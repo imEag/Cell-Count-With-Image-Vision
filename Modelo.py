e17a1dc9ac52b5d9826dded47988ec6298578e90
Emmanuel <emma145@hotmail.es>
2021-01-25 19:44:57 -0500

    se hizo la ventana principal con todas las opciones

--- Modelo.py
+++ Modelo.py
@@ -1,3 +1,13 @@
 import matplotlib as plt
 import cv2
+import numpy as np
+import matplotlib.pyplot as plt
 
+class sistema:
+    def __init__(self):
+        self.__grafica=None
+
+    def mostrarimg(self,nimagen):
+        img=cv2.imread('./Imagenes/'+ nimagen)
+        img=cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
+        return img
---
+++ Vista.py
@@ -0,0 +1,47 @@
+from PyQt5.QtWidgets import QMainWindow, QFileDialog, QDialog
+from PyQt5.QtGui import QIntValidator
+from PyQt5.QtWidgets import QApplication, QMessageBox, QVBoxLayout
+from PyQt5.uic import loadUi
+from matplotlib.backends.backend_qt5agg import FigureCanvasQTAgg as FigureCanvas
+
+import numpy as np
+from matplotlib.figure import Figure
+
+
+class Dibujo(FigureCanvas):
+    def __init__(self, parent = None, widht = 5, higth = 4, dpi = 40):
+    
+        self.figura = Figure(figsize=(widht, higth), dpi=dpi)
+        self.axes = self.figura.add_subplot(111)
+        FigureCanvas.__init__(self,self.figura)
+
+    def graficarimg(self,img):
+        self.axes.imshow(img)
+        self.axes.figure.canvas.draw()
+
+
+class Ventanappal(QMainWindow):
+    def __init__(self, parent=None):
+        super(Ventanappal,self).__init__(parent)
+        loadUi('Ventanappal.ui',self)
+        self.setup()
+
+    def setup(self):
+        self.mostrar.clicked.connect(self.mostrarimg)
+        self.cancelar.clicked.connect(self.cancelarimg)
+
+        self.sc = Dibujo(self.campo1)
+ 
+        self.Layout1.addWidget(self.sc)
+
+    def mostrarimg(self):
+        nimagen=self.nimagen.text()
+        img=self.__coord.mostrarimg(nimagen)
+        self.sc.graficarimg(img)
+
+
+    def cancelarimg(self):
+        self.close()
+    def setcoord(self,coord):
+        self.__coord=coord
+        

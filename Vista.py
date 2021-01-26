d8df7ff666d3861251aca86b6626dc804235a3da
Ruizxp <andres.ruizt@udea.edu.co>
2021-01-25 20:45:16 -0500

    Vista y ventana

--- Vista.py
+++ Vista.py
@@ -27,20 +27,59 @@ class Ventanappal(QMainWindow):
         self.setup()
 
     def setup(self):
-        self.mostrar.clicked.connect(self.mostrarimg)
-        self.cancelar.clicked.connect(self.cancelarimg)
 
-        self.sc = Dibujo(self.campo1)
+        self.edit_xi.setValidator(QIntValidator())
+        self.edit_xf.setValidator(QIntValidator())
+        self.edit_yi.setValidator(QIntValidator())
+        self.edit_xf.setValidator(QIntValidator())
+
+        self.boton_histograma.setEnabled(False)
+        self.boton_contar.setEnabled(False)
+        self.boton_cambios.setEnabled(False)
+
+        self.boton_cargar.clicked.connect(self.cargar_img)
+        self.cancelar.clicked.connect(self.cerrar)
+        self.boton_histograma.clicked.connect(self.graficar_histograma)
+        self.boton_contar.setEnabled(self.contar_celulas)
+        self.boton_cambios.setEnabled(self.graficar_cambios)
+        self.boton_equalizar.setEnabled(self.equalizar)
+
+        # self.sc = Dibujo(self.campo1)
  
-        self.Layout1.addWidget(self.sc)
+        # self.Layout1.addWidget(self.sc)
 
-    def mostrarimg(self):
-        nimagen=self.nimagen.text()
-        img=self.__coord.mostrarimg(nimagen)
-        self.sc.graficarimg(img)
+    # def mostrarimg(self):
+    #     nimagen=self.nimagen.text()
+    #     img=self.__coord.mostrarimg(nimagen)
+    #     self.sc.graficarimg(img)
+    def cargar_img(self):
+        archivo_cargado, _ = QFileDialog.getOpenFileName(self, "Abrir imagen","","Archivos jpg (*.jpg)","","Archivos png (*.png)")
+        if archivo_cargado !='':
+            self.boton_histograma.setEnabled(True)
+            self.boton_contar.setEnabled(True)
+            self.boton_cambios.setEnabled(True)
 
+        else:
+            msj = QMessageBox(self)
+            msj.setText('El archivo no se ha cargado\nVuelva a intentarlo')
+            msj.show()
 
-    def cancelarimg(self):
+    def graficar_histograma(self):
+        pass
+
+    def graficar_cambios(self):
+        xi=self.edit_xi.text()
+        xf=self.edit_xf.text()
+        yi=self.edit_yi.text()
+        yf=self.edit_xf.text()
+
+    def contar_celulas(self):
+        pass
+
+    def equalizar(self):
+        pass
+
+    def cerrar(self):
         self.close()
     def setcoord(self,coord):
         self.__coord=coord
